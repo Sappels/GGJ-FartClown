0b59b6b4857761f60f93de0e4e13f201712e20ed
srijan17 <srijan17@yahoo.co.in>
2024-01-27 17:14:16 +0100

    bug fixes

--- GGJ-FartClown/Assets/Scripts/ComboSpawner/ComboManager.cs
+++ GGJ-FartClown/Assets/Scripts/ComboSpawner/ComboManager.cs
@@ -33,16 +33,15 @@ public class ComboManager : MonoBehaviour
     {
 
         //CauseKeySpawner is called and array starts with 0
-        currIndex = -1;
+        // currIndex = -1;
         // GenerateCombo();
-        SpawnNextKey(FillSpeed);
+        // SpawnNextKey(FillSpeed);
     }
 
     public void GenerateCombo(int length, float newFillSpeed)
     {
         FillSpeed = newFillSpeed;
         // ReduceFillSpeed()
-        currIndex = 0;
         KeyCode[] AvailableKeys = new KeyCode[] { KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V };
         List<KeyCode> randomElements = new List<KeyCode>();
         for (int i = 0; i < length; i++)
@@ -52,13 +51,15 @@ public class ComboManager : MonoBehaviour
         }
 
         Combo = randomElements.ToArray();
+        currIndex = -1;
+        SpawnNextKey(FillSpeed);
     }
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(currIndex + "-" + Combo.Length);
         if (currIndex < Combo.Length && Input.GetKeyDown(Combo[currIndex]))
         {
+            Debug.Log("Got a new key");
             CalculateSpeed();
             stageRunner.EatPizzaSlice();
             keySpawner.CorrectKey();
@@ -70,7 +71,7 @@ public class ComboManager : MonoBehaviour
     public void CalculateSpeed()
     {
         float reactionTime = Time.time - lastSpawnTime;
-        Debug.Log("Reacted in " + reactionTime);
+        // Debug.Log("Reacted in " + reactionTime);
     }
     // public void VerifyKey(KeyCode keyDown)
     // {
@@ -86,7 +87,7 @@ public class ComboManager : MonoBehaviour
         currIndex++;
         if (currIndex < Combo.Length)
         {
-            Debug.Log("Spawning new key at index: " + currIndex + "" + Combo[currIndex] + string.Join(", ", Combo));
+            Debug.Log("Spawning new key at index: " + currIndex + "" + Combo[currIndex] + "==" + string.Join(", ", Combo));
             keySpawner.SpawnKey(Combo[currIndex].ToString(), FillSpeed);
         }
     }

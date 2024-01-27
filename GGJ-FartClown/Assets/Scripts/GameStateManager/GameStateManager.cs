eecfcc0bccb6cdbf23c97bb7c98e080d1084f08e
Sappels <58121086+Sappels@users.noreply.github.com>
2024-01-27 17:15:33 +0100

    Added stats, see description
    AddScore: Added methods for adding score, which gives bonus if the player is very fast
    MissedKey: Added a method for keeping track how many keys have been missed
    ResetKeysMissed: Added a method for resetting the keysMissed count when farting.
    GameOver: Added a method to call when the loss condition is met.

--- GGJ-FartClown/Assets/Scripts/GameStateManager/GameStateManager.cs
+++ GGJ-FartClown/Assets/Scripts/GameStateManager/GameStateManager.cs
@@ -1,3 +1,4 @@
+using Palmmedia.ReportGenerator.Core.CodeAnalysis;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,12 @@ public class GameStateManager : MonoBehaviour
 {
     [SerializeField] private TMPro.TMP_Text m_timerUI;
 
+    public int keysMissed = 0;
+    public float score;
+
     public static GameStateManager Instance { get; private set; }
 
-    public GameState currState = GameState.ENTRY;
+    //public GameState currState = GameState.ENTRY;
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -19,18 +23,45 @@ public class GameStateManager : MonoBehaviour
         {
             Instance = this;
         }
-
     }
+
+    public void AddScore(float reactionTime, float fillSpeed)
+    {
+        float threshold = fillSpeed * 0.25f;
+
+        if (reactionTime <= threshold)
+            score += 125;
+        else
+            score += 100;
+    }
+
+    public void MissedKey()
+    {
+        keysMissed++;
+        if (keysMissed >= 3) GameOver();
+    }
+
+    public void ResetKeysMissed()
+    {
+        keysMissed = 0;
+    }
+
+    public void GameOver()
+    {
+        //Call this method when a loss condition has been met
+        //Shows a Game Over screen with pizzas eaten, times farted and score
+    }
+
 
     // Start is called before the first frame update
 
-    public void YouWin()
-    {
-        GameStateManager.Instance.currState = GameState.WIN;
-        m_timerUI.text = "YOU Win!";
-        Debug.Log("You Win!");
-
-    }
+    //public void YouWin()
+    //{
+    //    GameStateManager.Instance.currState = GameState.WIN;
+    //    m_timerUI.text = "YOU Win!";
+    //    Debug.Log("You Win!");
+    //
+    //}
     // public void Update()
     // {
     //     if (currState == GameState.RUNNING)
@@ -47,20 +78,20 @@ public class GameStateManager : MonoBehaviour
     //     if (m_timer < 0)
     //         FailedStage();
     // }
-    public void YouLose()
-    {
-        GameStateManager.Instance.currState = GameState.LOOSE;
-        m_timerUI.text = "YOU LOOSE!";
-
-        Debug.Log("You Lose!");
-    }
+    //public void YouLose()
+    //{
+    //    GameStateManager.Instance.currState = GameState.LOOSE;
+    //    m_timerUI.text = "YOU LOOSE!";
+    //
+    //    Debug.Log("You Lose!");
+    //}
 
 
 }
-public enum GameState
-{
-    ENTRY,
-    WIN,
-    LOOSE,
-    RUNNING
-}
+//public enum GameState
+//{
+//    ENTRY,
+//    WIN,
+//    LOOSE,
+//    RUNNING
+//}
